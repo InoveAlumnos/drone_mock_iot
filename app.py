01497ffb75cd0cfaafc081a5e42ecd6190cd030c
inoveAlumnos <alumnos@inove.com.ar>
2022-06-10 16:42:12 -0300

    Initial ocmmit

---
+++ app.py
@@ -0,0 +1,128 @@
+'''
+Inove Drone Mock Python IoT
+---------------------------
+Autor: Inove Coding School
+Version: 1.0
+ 
+Descripcion:
+Se utiliza Flask para crear un generador de datos
+de telemetría simulando un Drone:
+- Motores
+- Luz ON/OFF
+- Acelerómetro
+- Giróscopo
+- GPS
+
+Ejecución: Lanzar el programa y abrir en un navegador la siguiente dirección URL
+https://IP:5006/
+'''
+
+__author__ = "Inove Coding School"
+__email__ = "alumnos@inove.com.ar"
+__version__ = "1.0"
+
+import traceback
+import json
+
+from flask import Flask, request, jsonify, render_template, redirect
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
+app = Flask(__name__)
+app.secret_key = 'ptSecret'
+app.config['SECRET_KEY'] = 'ptSecret'
+socketio = SocketIO(app)
+
+# ---- MQTT ----
+import paho.mqtt.client as mqtt
+client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    print("MQTT Conectado")
+    client.subscribe("actuadores/volar")
+    client.subscribe("actuadores/luces/#")
+    client.subscribe("actuadores/motores/#")
+
+def mqtt_connect():
+    if client.is_connected() is False:
+        try:
+            client.connect("localhost", 1883, 10)
+            print("Conectado al servidor MQTT")
+            client.loop_start()
+        except:
+            print("No pudo conectarse")
+
+
+def on_message(client, userdata, msg):
+    topic = str(msg.topic)
+    value = str(msg.payload.decode("utf-8"))
+    print(msg.topic, msg.payload.decode("utf-8"))
+    if topic == "actuadores/volar":
+        socketio.emit('volar', int(value))
+    
+    # NOTA: Podría mejorarse el manejo del ID
+    # utilizando regular expression (re)
+    # Se deja de esta manera para que se vea
+    # facil para el alumno
+    if topic == "actuadores/luces/1":
+        socketio.emit('luz_1', int(value))
+    if topic == "actuadores/motores/1":
+        socketio.emit('motor_1', int(value))
+    if topic == "actuadores/motores/2":
+        socketio.emit('motor_2', int(value))
+    if topic == "actuadores/motores/3":
+        socketio.emit('motor_3', int(value))
+    if topic == "actuadores/motores/4":
+        socketio.emit('motor_4', int(value))
+
+
+# ---- Endpoints ----
+@app.route('/')
+def home():
+    mqtt_connect()
+    return render_template('index.html')
+
+
+@app.route('/luz/1/<val>')
+def light(val):
+    socketio.emit('luz_1', int(val))
+    return f"luz: {val}"
+
+# ---- Web sockets contra el frontend ----
+@socketio.on('sensores_event')
+def ws_sensores_event(data):
+    pass
+    client.publish("sensores/inerciales", json.dumps(data["inerciales"]))
+    client.publish("sensores/gps", json.dumps(data["gps"]))
+
+
+@socketio.on('luz_event')
+def ws_luz_event(data):
+    # data --> estado de la luz_1 (0 o 1), pasar a int
+    client.publish("actuadores/luces/1", int(data))
+
+
+@socketio.on('volar_event')
+def ws_volar_event(data):
+    # data --> estado de la volar (0 o 1), pasar a int
+    client.publish("actuadores/volar", int(data))
+
+
+@socketio.on('motores_event')
+def ws_motores_event(data):
+    # data --> lista de estado de los motores, pasar a int c/u
+    for i in range(4):
+        client.publish(f"actuadores/motores/{i+1}", int(data[i]))
+
+
+@socketio.on('joystick_event')
+def ws_motores_event(data):
+    # data --> diccionario de estado del joystick, enviar como JSON string
+    client.publish(f"actuadores/joystick", json.dumps(data))
+
+
+if __name__ == "__main__":
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    app.run(debug=True, host="0.0.0.0", port=5006)
---
+++ static/js/app.js
@@ -0,0 +1,258 @@
+// ---- Estructura de datos ----
+let data = {
+    luz: 0,
+    volar: 0,
+    motores: [0, 0, 0, 0],
+    joystick: {x: 0, y: 0},
+    inerciales: {heading: 0, accel: 0},
+    gps: {latitude: -34.55, longitude: -58.496},
+    monitoreo: {temp: 0, ram:0, cpu:0}   
+}
+
+let socket_connected = false;
+
+// ---- Elementos del HTML ----
+const drone = document.getElementById("drone");
+const motor1 = document.querySelector("#motor1");
+const motor2 = document.querySelector("#motor2");
+const motor3 = document.querySelector("#motor3");
+const motor4 = document.querySelector("#motor4");
+
+const light = document.getElementById("droneLight");
+
+const slight = document.querySelector("#slight");
+light.style.opacity = 0;
+
+const m1 = document.querySelector("#sM1");
+const m2 = document.querySelector("#sM2");
+const m3 = document.querySelector("#sM3");
+const m4 = document.querySelector("#sM4");
+
+m1.disabled = true;
+m2.disabled = true;
+m3.disabled = true;
+m4.disabled = true;
+
+// --- Funciones de ayuda ----
+function updateEngineState(state) {
+    if(state == true) {
+        data.volar = 1;
+        data.motores[0] = 1;
+        m1.checked = true;
+        m1.disabled = false;
+        data.motores[1] = 1;
+        m2.checked = true;
+        m2.disabled = false;
+        data.motores[2] = 1;
+        m3.checked = true;
+        m3.disabled = false;
+        data.motores[3] = 1;
+        m4.checked = true;
+        m4.disabled = false;
+    } else {
+        data.volar = 0;
+        data.motores[0] = 0;
+        m1.checked = false;
+        m1.disabled = true;
+        data.motores[1] = 0;
+        m2.checked = false;
+        m2.disabled = true;
+        data.motores[2] = 0;
+        m3.checked = false;
+        m3.disabled = true;
+        data.motores[3] = 0;
+        m4.checked = false;
+        m4.disabled = true;
+    }
+}
+
+function rotate() {
+    x = data.joystick.y > 0? data.joystick.y * 60 : 0;
+    y = data.joystick.x * 60 + 180;
+    z = (-data.inerciales.heading);
+    drone.style.webkitTransform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+    drone.style.MozTransform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+    drone.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+}
+
+function update() {
+    data.motores[0] == true ? motor1.classList.add("propeller--on") : motor1.classList.remove("propeller--on");
+    data.motores[1] == true ? motor2.classList.add("propeller--on") : motor2.classList.remove("propeller--on");
+    data.motores[2] == true ? motor3.classList.add("propeller--on") : motor3.classList.remove("propeller--on");
+    data.motores[3] == true ? motor4.classList.add("propeller--on") : motor4.classList.remove("propeller--on");
+}
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+function sendActuadorUpdate(actuador) {
+    if (socket_connected == true){
+        if(actuador == "volar") {
+            socket.emit("volar_event", data.volar);
+        }
+        if(actuador == "luz") {
+            socket.emit("luz_event", data.luz);
+        }
+        if(actuador == "motores") {
+            socket.emit("motores_event", data.motores);
+        }
+        if(actuador == "joystick") {
+            socket.emit("joystick_event", data.joystick);
+        }
+    }
+}
+
+// ---- Instanciar elementos HTML y conectar eventos ----
+
+// https://www.codehim.com/text-input/round-range-slider-html-control-in-jquery/#:~:text=The%20plugin%2C%20%E2%80%9CroundSlider%E2%80%9D%20is,build%20with%20CSS%20and%20JavaScript.
+// https://roundsliderui.com/document.html
+$("#heading").roundSlider({
+    sliderType: "min-range",
+    value: 0,
+    max : 360,
+    startAngle: "+90",
+    endAngle : "+360",
+    drag: function(e) {
+        var angle = e.value;
+        data.inerciales.heading = angle;
+        rotate();
+    }
+});
+$("#accel").roundSlider({
+    sliderType: "min-range",
+    value: 0,
+    max : 5,
+    min : -5,
+    step: 0.2,        
+    startAngle: "-90",
+    drag: function(e) {
+        var angle = e.value;
+        data.inerciales.accel = angle;
+        rotate();
+    }
+});
+
+document.querySelector("#latitude").value = data.gps.latitude;
+document.querySelector("#latitude").onchange = (e) => {
+    if(isNumeric(e.value) == true) {
+        data.gps.latitude = parseFloat(e.value);
+    }
+};
+
+document.querySelector("#longitude").value = data.gps.longitude;
+document.querySelector("#longitude").onchange = (e) => {
+    if(isNumeric(e.value) == true) {
+        data.gps.longitude = parseFloat(e.value);
+    }
+};
+
+joystick.ondrag = function(){
+    data.joystick.x = this.shaft.current.abs_vector.x;
+    data.joystick.y = this.shaft.current.abs_vector.y;
+    rotate();
+    sendActuadorUpdate("joystick");
+}
+
+m1.onchange = (e) => {
+    data.motores[0] = e.target.checked;
+    update();
+    sendActuadorUpdate("motores");
+}
+m2.onchange = (e) => {
+    data.motores[1] = e.target.checked;
+    update();
+    sendActuadorUpdate("motores");
+}
+m3.onchange = (e) => {
+    data.motores[2] = e.target.checked;
+    update();
+    sendActuadorUpdate("motores");
+}
+m4.onchange = (e) => {
+    data.motores[3] = e.target.checked;
+    update();
+    sendActuadorUpdate("motores");
+}
+
+slight.onchange = (e) => {
+    const val = e.target.checked ? 1 : 0;
+    data.luz = val;
+    light.style.opacity = val;
+    sendActuadorUpdate("luz");
+}
+const sengine = document.querySelector("#sengine");
+sengine.onchange = (e) => {
+    updateEngineState(e.target.checked);
+    update();
+    sendActuadorUpdate("volar");
+    sendActuadorUpdate("motores");
+}
+
+// ---- Web sockets contra el backend ----
+let socket = io();
+socket.on("connect", function() {
+    socket_connected = true;
+    socket.on('luz_1', function (msg) {
+        const val = Number(msg);
+        data.luz = val;
+        light.style.opacity = val;
+        slight.checked = val;
+    });
+    socket.on('volar', function (msg) {
+        const val = Number(msg);
+        sengine.checked = val;
+        updateEngineState(val);
+        update();
+    });
+    socket.on('motor_1', function (msg) {
+        // Si el está activado el vuelo
+        // permito actualizar el estado del motor
+        if(sengine.checked == true) {
+            const val = Number(msg);
+            data.motores[0] = msg;
+            m1.checked = msg;
+            update();
+        }
+    });
+    socket.on('motor_2', function (msg) {
+        // Si el está activado el vuelo
+        // permito actualizar el estado del motor
+        if(sengine.checked == true) {
+            const val = Number(msg);
+            data.motores[1] = msg;
+            m2.checked = msg;
+            update();
+        }
+    });
+    socket.on('motor_3', function (msg) {
+        // Si el está activado el vuelo
+        // permito actualizar el estado del motor
+        if(sengine.checked == true) {
+            const val = Number(msg);
+            data.motores[2] = msg;
+            m3.checked = msg;
+            update();
+        }
+    });
+    socket.on('motor_4', function (msg) {
+        // Si el está activado el vuelo
+        // permito actualizar el estado del motor
+        if(sengine.checked == true) {
+            const val = Number(msg);
+            data.motores[3] = msg;
+            m4.checked = msg;
+            update();
+        }
+    });
+});
+
+
+(function my_func() {
+    if (socket_connected == true){
+        socket.emit("sensores_event", data);
+    }
+    setTimeout( my_func, 500 );
+})();
